759e0a8a4f7fb99f83096275ccc9ad72e074f42e
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-03-12 02:27:15 -0800

    commited

---
+++ myjava/src/Pattern_Question/Solid_Rectangle.java
@@ -0,0 +1,28 @@
+package Pattern_Question;
+import java.util.*;
+public class Solid_Rectangle {
+//	                             -----> column
+//								|	* * * * *  
+//								|	* * * * *
+//							   row  * * * * * 
+//								    * * * * * 
+	public static void main(String args[]) {
+		
+		Scanner sc=new Scanner(System.in);
+		int row;
+		int colm;
+		
+		System.out.print("enter row number in pattern: ");
+		row=sc.nextInt();
+		System.out.print("\nenter coloum numbers in pattern: ");
+		colm=sc.nextInt();
+		
+		for(int i=1;i<=colm;i++) {
+			for(int j=1;j<=row;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+	}
+}
